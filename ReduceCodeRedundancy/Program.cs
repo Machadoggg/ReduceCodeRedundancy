389d8959a3197f98acc34e58dc3d5840363a7f88
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-10-23 11:42:05 -0500

    Use ternary operator to reduce code

--- ReduceCodeRedundancy/Program.cs
+++ ReduceCodeRedundancy/Program.cs
@@ -2,13 +2,17 @@
 int age = 40;
 string message = "";
 
-if (age >= 18)
-{
-    message = "Es mayor de edad";
-}
-else 
-{
-    message = "Es menor de edad";
-}
+//if (age >= 18)
+//{
+//    message = "Es mayor de edad";
+//}
+//else 
+//{
+//    message = "Es menor de edad";
+//}
+
+// use ternary operator if the answer is a binary scenario
+// obtain var value (message)
+message = (age >= 18) ? "Es mayor de edad" : "Es menor de edad";
 
 Console.WriteLine(message);
