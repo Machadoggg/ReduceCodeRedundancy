c6f48c8cc0f5d2dc0a3b850d4c7a46703a6dfc68
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-10-23 11:52:17 -0500

    Scenario with var null

--- ReduceCodeRedundancy/Program.cs
+++ ReduceCodeRedundancy/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel;
+
 int age = 40;
 string message = "";
 
@@ -18,3 +20,17 @@ message = (age >= 18) ? "Es mayor de edad" : "Es menor de edad";
 Console.WriteLine(message);
 
 //-----------------------------------------------------------------------------//
+
+string? category = null;
+string name = "";
+
+if (category != null)
+{
+    name = category;
+}
+else
+{
+    name = "Otra";
+}
+
+Console.WriteLine(name);
