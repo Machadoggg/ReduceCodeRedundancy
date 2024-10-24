662169da12fc6035f11c4232e746d852c5dc67e9
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-10-23 22:46:37 -0500

    List in second list add all names init with (A)

--- ReduceCodeRedundancy/Program.cs
+++ ReduceCodeRedundancy/Program.cs
@@ -2,71 +2,96 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-
-//-------------------------TERNARY OPERATOR--------------------------------------//
-int age = 40;
-string message = "";
-
-//if (age >= 18)
-//{
-//    message = "Es mayor de edad";
-//}
-//else 
-//{
-//    message = "Es menor de edad";
-//}
-
-// use ternary operator (? / true : false) if the answer is a binary scenario
-// obtain var value (message)
-message = (age >= 18) ? "Es mayor de edad" : "Es menor de edad";
-
-Console.WriteLine(message);
-
-
-
-
-//-------------------------Null-Coalescing Operator--------------------------------------//
-
-//string? category = null;
-//string name = "";
-
-//if (category != null)
-//{
-//    name = category;
-//}
-//else
-//{
-//    name = "Otra";
-//}
-
-string? category = null;
-
-string name = category ?? "Otro";
-
-Console.WriteLine(name);
-
-
-
-
-
-//--------------------------Evaluate Car null-----------------------------------//
-
-
-//Car car = null;
-
-//if (car != null)
-//{
-//    Console.WriteLine(car.Brand);
-//}
-//record Car(string Brand, string Model);
-
-// Use NULL conditional (?)
-// to evaluate if car is null
-// if encadenate Console.WriteLine(car?.Brand?.?. ...);
-Car car = null;
-
-if (car != null)
+class Program
 {
-    Console.WriteLine(car?.Brand);
+    static void Main(string[] args)
+    {
+        //-------------------------TERNARY OPERATOR--------------------------------------//
+        int age = 40;
+        string message = "";
+
+        //if (age >= 18)
+        //{
+        //    message = "Es mayor de edad";
+        //}
+        //else 
+        //{
+        //    message = "Es menor de edad";
+        //}
+
+        // use ternary operator (? / true : false) if the answer is a binary scenario
+        // obtain var value (message)
+        message = (age >= 18) ? "Es mayor de edad" : "Es menor de edad";
+
+        Console.WriteLine(message);
+
+
+
+
+        //-------------------------Null-Coalescing Operator--------------------------------------//
+
+        //string? category = null;
+        //string name = "";
+
+        //if (category != null)
+        //{
+        //    name = category;
+        //}
+        //else
+        //{
+        //    name = "Otra";
+        //}
+
+        string? category = null;
+
+        string name = category ?? "Otro";
+
+        Console.WriteLine(name);
+
+
+
+
+
+        //--------------------------Evaluate Car null-----------------------------------//
+
+
+        //Car car = null;
+
+        //if (car != null)
+        //{
+        //    Console.WriteLine(car.Brand);
+        //}
+        //record Car(string Brand, string Model);
+
+        // Use NULL conditional (?)
+        // to evaluate if car is null
+        // if encadenate Console.WriteLine(car?.Brand?.?. ...);
+        Car car = null;
+
+        if (car != null)
+        {
+            Console.WriteLine(car?.Brand);
+        }
+
+
+        //--------------------------2 List in second list add all names init with (A)-----------------------------------//
+
+        List<string> names = new()
+        {
+            "Ana", "Juan", "Alejandro"
+        };
+
+        List<string> names2 = new();
+
+        foreach (string name2 in names)
+        {
+            if (name2.ToUpper()[0] == 'A')
+            {
+                names2.Add(name2);
+            }
+        }
+
+        return;
+    }
 }
 record Car(string Brand, string Model);
